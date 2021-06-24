ca3f28b88ed684164d650b96c8c301e90cf1a702
Your Name <you@example.com>
2021-06-24 17:30:53 +0300

    first commit

--- car/models.py
+++ car/models.py
@@ -1,13 +1,12 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
-TR = 'Tr'
-OR = 'Or'
-PC = 'Pc'
 CAR_TYPES = [
-    (TR, 'Truck'),
-    (OR, 'Off-roader'),
-    (PC, 'Passenger car'),
+    ('Truck', 'Truck'),
+    ('Off-roader', 'Off-roader'),
+    ('Passenger car', 'Passenger car'),
 ]
 
 
@@ -17,7 +16,7 @@ class Car(models.Model):
     car_model = models.ForeignKey("car.CarModel", on_delete=models.SET_NULL, null=True)
     car_colour = models.ForeignKey("car.CarColour", blank=True, null=True, on_delete=models.SET_NULL)
     car_release_date = models.ForeignKey("car.CarReleaseDate", blank=True, null=True, on_delete=models.SET_NULL)
-    car_type = models.ForeignKey("car.CarType", blank=True, null=True, on_delete=models.SET_NULL, choices=CAR_TYPES)
+    car_type = models.ForeignKey("car.CarType", blank=True, null=True, on_delete=models.SET_NULL)
 
     def company(self):
         return self.car_model.company
@@ -56,10 +55,10 @@ class CarColour(models.Model):
 
 class CarReleaseDate(models.Model):
 
-    date = models.CharField(max_length=100)
+    date = models.DateField("Car release date", auto_now=False, auto_now_add=False)
 
     def __str__(self):
-        return self.date
+        return str(self.date)
 
     class Meta:
         verbose_name_plural = "Cars release dates"
@@ -67,7 +66,7 @@ class CarReleaseDate(models.Model):
 
 class CarType(models.Model):
 
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=CAR_TYPES)
 
     def __str__(self):
         return self.type
